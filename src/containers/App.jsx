5c752f0e53fc19b16fa054e9f01af897017fbb91
Marcio Hariki <marciohariki@gmail.com>
2019-10-07 03:42:32 -0300

    add travis configuration

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import { Route, Router, Switch } from 'react-router-dom'
-import './App.css';
+import { Redirect, Route, Router, Switch } from 'react-router-dom'
+import './App.css'
 
 import history from '../core/history'
 import SpotifyCallback from 'components/spotify-auth/spotify-callback/SpotifyCallback'
 import UserProfile from './profile'
 import Player from './player'
 import Home from './home'
-
 
 class App extends React.Component {
   render () {
@@ -19,6 +18,7 @@ class App extends React.Component {
             <Route path="/spotify-callback" component={SpotifyCallback} />
             <Route path="/profile" component={UserProfile} />
             <Route path="/player" component={Player} />
+            <Redirect path="/" />
           </Switch>
         </div>
       </Router>
